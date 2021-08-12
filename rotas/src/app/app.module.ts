3fdb0e04fefd5b76ca8e5f025b7647a5b53bab66
rnicolino <rodrigo_nicolino@hotmail.com>
2021-08-12 18:08:56 -0300

    202108121

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CursosGuard } from './guards/cursos.guard';
 //import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 //import { CursosModule } from './cursos/cursos.module';
 //import { AlunosComponent } from './alunos/alunos.component';
+//import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [
