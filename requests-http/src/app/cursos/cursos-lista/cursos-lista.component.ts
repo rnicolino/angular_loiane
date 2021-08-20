b44b928b07bf014d5c602c43b75a71beba95f2ad
rnicolino <rodrigo_nicolino@hotmail.com>
2021-08-20 20:31:38 -0300

    HTTP - 20210820

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { empty, Observable, Subject } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -14,14 +15,41 @@ export class CursosListaComponent implements OnInit {
   //cursos: Curso[] = [];
 
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
 
     //this.service.list().subscribe(dados => this.cursos = dados);
+    this.onRefresh();
+    
+  }
 
-    this.cursos$ = this.service.list();
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(erros => {
+        console.error(erros);
+        this.error$.next(true);
+        return empty();
+      })
+    );
+
+    this.service.list()
+    .pipe(
+      //map(),
+      //tap(),
+      //switchMap(),
+      catchError(error => empty())
+    )
+    .subscribe(
+      dados => {
+        console.log(dados)
+      },
+      //error => console.log(error),
+      //() => console.log("Observable compleo!")
+    );
   }
 
 }
